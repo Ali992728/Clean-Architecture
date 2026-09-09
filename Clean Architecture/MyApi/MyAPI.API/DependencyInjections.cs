bf7584f608d086127df699ae5115111df641b5b5
Ali Abrar Khan <abrar9927@gmail.com>
2026-09-09 23:59:50 +0600

    updated

---
+++ Clean Architecture/MyApi/MyAPI.API/DependencyInjections.cs
@@ -0,0 +1,17 @@
+﻿using MyAPI.Application;
+using MyAPI.Core;
+using MyAPI.Infrastructure;
+
+namespace MyAPI.API
+{
+    public static class DependencyInjections
+    {
+        public static IServiceCollection AddAPIDI(this IServiceCollection services)
+        {
+            services.AddCoreDI();
+            services.AddApplicationDI();
+            services.AddInfrastructureDI();
+            return services;
+        }
+    }
+}
